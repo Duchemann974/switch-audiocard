5762265ef85a14858de9710c41948d4901b8776e
Duchemann974 <chris974___@hotmail.fr>
2025-11-12 10:30:38 +0100

    ajout du fichier log.txt et ajout feature log

--- main.ps1
+++ main.ps1
@@ -10,7 +10,16 @@ if (!($module)) {
 $usbDevice = Get-PnpDevice | Where-Object { $_.FriendlyName -like 'Haut-parleurs (C-Media USB Audio Device   )' } -Verbose
 
 #Activation du casque si detecté
-if ($usbDevice) {
+if ($usbDevice.Status -eq 'OK') {
     Write-Host " Casque USB, bascule vers $($usbDevice.FriendlyName)" -ForegroundColor Green
+    Write-Output " $(get-date) - Bascule OK" >> .\log.txt
     Set-AudioDevice -ID '{0.0.0.00000000}.{e4717860-080c-4853-9200-1e77c0dd46ef}' 
-} 
+} else {
+    Write-Output " $(get-date) - Bascule non effectué, casque non branché" >> .\log.txt
+}
+
+
+
+#C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+
+#-File "C:\Users\chris\Documents\switch-audiocard\main.ps1"
