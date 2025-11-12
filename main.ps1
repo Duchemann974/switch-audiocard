e3f09e332431e8a41bd44a16406b1e104634eb49
Duchemann974 <chris974___@hotmail.fr>
2025-11-12 20:26:20 +0100

    modification path logs

--- main.ps1
+++ main.ps1
@@ -1,3 +1,5 @@
+$path_logs = "C:\Users\chris\Documents\switch-audiocard\log.txt"
+
 # Vérification de la prescence du module "AudioDeviceCmdlets"
 $module = Get-Module AudioDeviceCmdlets -Verbose
 
@@ -12,10 +14,10 @@ $usbDevice = Get-PnpDevice | Where-Object { $_.FriendlyName -like 'Haut-parleurs
 #Activation du casque si detecté
 if ($usbDevice.Status -eq 'OK') {
     Write-Host " Casque USB, bascule vers $($usbDevice.FriendlyName)" -ForegroundColor Green
-    Write-Output " $(get-date) - Bascule vers le casque OK" >> .\log.txt
+    Write-Output " $(get-date) - Bascule vers le casque OK" >> $path_logs
     Set-AudioDevice -ID '{0.0.0.00000000}.{e4717860-080c-4853-9200-1e77c0dd46ef}' 
 } else {
-    Write-Output " $(get-date) - Bascule vers le casque non effectuée, casque non branché" >> .\log.txt
+    Write-Output " $(get-date) - Bascule vers le casque non effectuée, casque non branché" >> $path_logs
 }
 
 
