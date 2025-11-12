1a1674c1fbfbb6c66c9e480119b78bf046f7c853
Duchemann974 <chris974___@hotmail.fr>
2025-11-12 13:01:24 +0100

    modification contenu log

--- main.ps1
+++ main.ps1
@@ -12,10 +12,10 @@ $usbDevice = Get-PnpDevice | Where-Object { $_.FriendlyName -like 'Haut-parleurs
 #Activation du casque si detecté
 if ($usbDevice.Status -eq 'OK') {
     Write-Host " Casque USB, bascule vers $($usbDevice.FriendlyName)" -ForegroundColor Green
-    Write-Output " $(get-date) - Bascule OK" >> .\log.txt
+    Write-Output " $(get-date) - Bascule vers le casque OK" >> .\log.txt
     Set-AudioDevice -ID '{0.0.0.00000000}.{e4717860-080c-4853-9200-1e77c0dd46ef}' 
 } else {
-    Write-Output " $(get-date) - Bascule non effectué, casque non branché" >> .\log.txt
+    Write-Output " $(get-date) - Bascule vers le casque non effectuée, casque non branché" >> .\log.txt
 }
 
 
