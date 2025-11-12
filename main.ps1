73dc0b6bef52f2b1e7f84bc07f780373023abf42
Duchemann974 <chris974___@hotmail.fr>
2025-11-12 08:55:45 +0100

    ajout import et install module features

--- main.ps1
+++ main.ps1
@@ -1,9 +1,16 @@
-Install-Module -Name AudioDeviceCmdlets -Scope CurrentUser
+
+$module = Get-Module AudioDeviceCmdlets -Verbose
+
+if (!($module)) {
+    Install-Module -Name AudioDeviceCmdlets -Scope CurrentUser -Verbose
+    Import-Module -Name AudioDeviceCmdlets -Verbose
+}
+
 
 # Vérifie si le périphérique USB est connecté
-$usbDevice = Get-PnpDevice | Where-Object { $_.FriendlyName -like 'Haut-parleurs (C-Media USB Audio Device   )' }
+$usbDevice = Get-PnpDevice | Where-Object { $_.FriendlyName -like 'Haut-parleurs (C-Media USB Audio Device   )' } -Verbose
 
 if ($usbDevice) {
-    Write-Host "🎧 Casque USB détecté — bascule vers $($usbDevice.FriendlyName)"
-    Set-AudioDevice -ID "{0.0.0.00000000}.{e4717860-080c-4853-9200-1e77c0dd46ef}"
+    Write-Host "🎧 Casque USB détecté — bascule vers $($usbDevice.FriendlyName)" -ForegroundColor Green
+    Set-AudioDevice -ID "{0.0.0.00000000}.{e4717860-080c-4853-9200-1e77c0dd46ef}" 
 } 
